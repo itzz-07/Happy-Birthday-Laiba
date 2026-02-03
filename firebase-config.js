69f99d64c82811cc04e38f13ca117a1b5488c099
itzz-07 <mdanas0600@gmail.com>
2026-02-03 12:47:55 +0530

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,1 +1,14 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCLE0V86QwtGM4T-xR9VNQh8a6eM6lQCrA",
+  authDomain: "laiba-00.firebaseapp.com",
+  projectId: "laiba-00",
+  storageBucket: "laiba-00.firebasestorage.app",
+  messagingSenderId: "483377362620",
+  appId: "1:483377362620:web:8f52dc9fc00ee232e0c2b9",
+  measurementId: "G-Y421LWYG6T"
+};
 
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+const database = firebase.database();
